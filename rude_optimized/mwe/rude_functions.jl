a477d9f872e42b294aa7fb84e95a81ddffb861b4
erlebach <gordon.erlebach@gmail.com>
2023-03-02 14:29:05 -0500

    Code contained in a single file. Same error.

--- rude_optimized/mwe/rude_functions.jl
+++ rude_optimized/mwe/rude_functions.jl
@@ -1,64 +1,59 @@
-
-function single_run(dct)
-
-	# Define the simple shear deformation protocol (all components except v21(t))
-    v11(t) = 0
-    
-    # Initial conditions and time span
-	tspan = (0.0f0, 1.0f0)
-
-	σ0 = SA[0.  0.  0.; 0.  0.  0.; 0.  0.  0.]  # Generates set_index! error
-	v21_protoc = [ (t) -> 0. for i in 1:8]
-
-	# Linear array of size 9
-	protocols = [ [v11,v11,v11,  v21_protoc[1], v11,v11,v11,v11,v11] ]
-    nb_protoc = 1
-    
-	p_system = Float32[0., 0.]  # change 2023-02-26_15:40
-    
-	θi = [0.] #p_model
-
-	# Loss function closure (first parameter: concatenate all parameters)
-	loss_fn(θ) = loss_univ([θ; p_system], protocols[1], tspan, σ0, [0.], 1) #dct)
-        adtype = Optimization.AutoZygote()
-        optf = Optimization.OptimizationFunction((x,p)->loss_fn(x),  adtype)
-        optprob = Optimization.OptimizationProblem(optf, θi)  
-		parameter_res = Optimization.solve(optprob, Optimisers.AMSGrad(), sensealg=ReverseDiffVJP(true), allow_f_increases=false, maxiters=20)
-    #end
-end
+#using DiffEqFlux, Flux, Optim, DifferentialEquations, LinearAlgebra, OrdinaryDiffEq, DelimitedFiles
+using DiffEqFlux, Flux, Optim, DifferentialEquations# ,LinearAlgebra #, OrdinaryDiffEq
+using Optimization, OptimizationOptimisers, OptimizationOptimJL
+using Zygote
+using StaticArrays
 
 
 function dudt_univ_opt(u, p, t)
 	du = @SMatrix [0. 0. 0.; 0. 0. 0.; 0. 0. 0.]  # Static array
 end
 
-function ensemble_solve(θ, ensemble, protocols, tspans, σ0, trajectories)
-	dudt_protocol(u, p, t) = dudt_univ_opt(u, p ,t)
+function single_run()
+	# Define the simple shear deformation protocol (all components except v21(t))
+    v11(t) = 0
+    
+    # Initial conditions and time span
+	tspan = (0.0f0, 1.0f0)
+	σ0 = SA[0.  0.  0.; 0.  0.  0.; 0.  0.  0.]  # Generates set_index! error
+
+	p_system = Float32[0., 0.]  # change 2023-02-26_15:40
+	θi = [0.] 
+
+	# Loss function closure (first parameter: concatenate all parameters)
+	loss_fn(θ) = loss_univ([θ; p_system], tspan, σ0, 1) 
+        adtype = Optimization.AutoZygote()
+        optf = Optimization.OptimizationFunction((x,p)->loss_fn(x),  adtype)
+        optprob = Optimization.OptimizationProblem(optf, θi)  
+		parameter_res = Optimization.solve(optprob, Optimisers.AMSGrad(), sensealg=ReverseDiffVJP(true), allow_f_increases=false, maxiters=20)
+end
+
+function ensemble_solve(θ, ensemble, tspans, σ0, trajectories)
 	θ = [1]
-    prob = ODEProblem(dudt_protocol, σ0, tspans[1], θ)
+    prob = ODEProblem(dudt_univ_opt, σ0, tspans[1], θ)
 
     function prob_func(prob, i, repeat)
-			dudt_remade(u, p, t) = dudt_univ_opt(u, p ,t) # >>> ERROR
+			dudt_remade(u, p, t) = dudt_univ_opt(u, p ,t) 
         remake(prob, f=dudt_remade, tspan=tspans[i])
     end
 
-	# EnsembleProblem: https://docs.sciml.ai/DiffEqDocs/dev/features/ensemble/
-    prob_func1(prob, i, repeat) = prob_func(prob, i, repeat)
-    ensemble_prob = EnsembleProblem(prob, prob_func=prob_func1)
-	saveat = 0.2
-	sim = solve(ensemble_prob, Tsit5(), ensemble, trajectories=trajectories, saveat=0.2)  # original
+    ensemble_prob = EnsembleProblem(prob, prob_func=prob_func)
+	sim = solve(ensemble_prob, Tsit5(), ensemble, trajectories=trajectories, saveat=0.2)  
 end
 
-function loss_univ(θ,protocols,tspans,σ0,σ12_all,trajectories)
+function loss_univ(θ, tspans, σ0, trajectories)
+	println("ENTER loss_univ")
     loss = 0
-    results = ensemble_solve(θ, EnsembleThreads(), protocols, tspans, σ0, trajectories)
-    for k = range(1,trajectories,step=1)
-		xxx = results[k][2,1,:]  # 2nd row, 1st column: σ12 = σ21
-		σ12_pred = results[k][2,1,:]  # 2nd row, 1st column: σ12 = σ21
-        loss += sum(abs2, σ12_pred )  # remove Giesekus solution for testing
-    end
-    loss += 0.01*norm(θ,1)   # L1 norm for sparsification
+    results = ensemble_solve(θ, EnsembleThreads(), tspans, σ0, trajectories)
+	println("results: ", results |> size)
+	println("results[1]: ", results[1] |> size)
+	xxx = results[1][2,1,:]  # 2nd row, 1st column: σ12 = σ21
+	println("results[1][2,1,:]: ==> ", xxx)
+	σ12_pred = results[1][2,1,:]  # 2nd row, 1st column: σ12 = σ21
+	println("σ12_pred: ", σ12_pred)
+    loss += sum(abs2, σ12_pred )  # Remove this line and error changes
+	println("before exiting loss_univ, loss: ", loss)
     return loss
 end
 
-
+single_run()
